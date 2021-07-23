013a8028b3ffbd3b056de920ace09e7c3d1b8112
gearmagicss <87186095+gearmagicss@users.noreply.github.com>
2021-07-23 14:18:16 +0700

    Delete src/net/Indyuce/mmoitems directory

--- src/net/Indyuce/mmoitems/comp/placeholder/PlaceholderParser.java
+++
@@ -1,7 +0,0 @@
-package net.Indyuce.mmoitems.comp.placeholder;
-
-import org.bukkit.OfflinePlayer;
-
-public interface PlaceholderParser {
-	public String parse(OfflinePlayer player, String string);
-}
