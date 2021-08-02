d6912aaccb9b25ce2adb64bfecab0a1637420d57
gearmagicss <87186095+gearmagicss@users.noreply.github.com>
2021-08-02 18:01:42 +0700

    Add files via upload

---
+++ net/indyuce/MMoitems/spigot-api/org/yaml/snakeyaml/reader/ReaderException.java
@@ -0,0 +1,39 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package org.yaml.snakeyaml.reader;
+
+import org.yaml.snakeyaml.error.YAMLException;
+
+public class ReaderException extends YAMLException
+{
+    private static final long serialVersionUID = 8710781187529689083L;
+    private final String name;
+    private final char character;
+    private final int position;
+    
+    public ReaderException(final String name, final int position, final char character, final String message) {
+        super(message);
+        this.name = name;
+        this.character = character;
+        this.position = position;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public char getCharacter() {
+        return this.character;
+    }
+    
+    public int getPosition() {
+        return this.position;
+    }
+    
+    @Override
+    public String toString() {
+        return "unacceptable character '" + this.character + "' (0x" + Integer.toHexString(this.character).toUpperCase() + ") " + this.getMessage() + "\nin \"" + this.name + "\", position " + this.position;
+    }
+}
