5362aa33c5bf7961fe1847155a335a78b09db4d3
gearmagicss <87186095+gearmagicss@users.noreply.github.com>
2021-08-02 18:59:41 +0700

    Delete net/indyuce/MMoitems/spigot-api directory

--- net/indyuce/MMoitems/spigot-api/org/bukkit/entity/Blaze.java
+++
@@ -1,9 +0,0 @@
-// 
-// Decompiled by Procyon v0.5.36
-// 
-
-package org.bukkit.entity;
-
-public interface Blaze extends Monster
-{
-}
--- net/indyuce/MMoitems/spigot-api/org/bukkit/inventory/AnvilInventory.java
+++
@@ -1,9 +0,0 @@
-// 
-// Decompiled by Procyon v0.5.36
-// 
-
-package org.bukkit.inventory;
-
-public interface AnvilInventory extends Inventory
-{
-}
